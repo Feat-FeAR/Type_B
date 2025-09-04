07639a5cbd3629df578040f8652928d94074d533
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2025-09-04 15:35:42 +0200

    minor: cleaned pubs.sh script header

--- pubs.sh
+++ pubs.sh
@@ -1,85 +1,19 @@
 #!/bin/bash
 
 # ==============================================================================
-#  Script Title
+#  Pubs - Quickly generates a report of my closed projects and publications 
 # ==============================================================================
+ver="0.2.0"
 
 # --- General settings and variables -------------------------------------------
 
-# The so-called strict mode (see https://mywiki.wooledge.org/BashFAQ/105)
+# Strict mode
 set -e           # "exit-on-error" shell option
 set -u           # "no-unset" shell option
 set -o pipefail  # exit on within-pipe error
 
-# ==============================================================================
-# NOTE on -e option
-# -----------------
-# `set -e` immediately stops the execution of a script if a command has non-zero
-# exit status. This is the opposite of the default shell behavior, which is to
-# ignore errors in scripts. The problem is that a non-zero exit code not always
-# means an error. In particular, if you use `grep` (or `pgrep`) and do NOT
-# consider finding no match as an error, you need to use the following syntax
-#
-#     grep "<expression>" "<target>" || [[ $? == 1 ]]
-#
-# to prevent `grep` from causing premature termination of the script.
-# This works since `[[ $? == 1 ]]` is executed if and only if `grep` fails (even
-# when `-e` option is active!) and, according to POSIX manual, `grep` exit code
-#   1 means "no lines selected";
-#   > 1 means an error.
-#
-# For the same reason, if you need to use `ls` you have to do this 
-#
-#      ls "<files>" 2> /dev/null || [[ $? == 2 ]]
-#
-# However, for these purposes it is recommended to use `find`, which does not
-# suffer from such a problem, since it returns an error only when the target
-# directory is not found (which is a much more unlikely event). When even the
-# existence of the searching location is uncertain, you can use:
-#
-#     find "<target_dir>" -type f -iname "<files>" 2> /dev/null || [[ $? == 1 ]]
-#
-# The same is true for `which`:
-#
-#     which <command> 2> /dev/null || [[ $? == 1 ]]
-#
-# In general, remember that enclosing a command within a conditional block
-# allows excluding it from the `set -e` behavior. E.g.,
-#
-#     if which <command> > /dev/null 2>&1; then ...; fi
-#
-# is fine, even without `|| [[ $? == 1 ]]`.
-#
-# NOTE on -o pipefail option
-# --------------------------
-# By default, the exit status of a pipe of many commands is the exit status of
-# the last (rightmost) command in the pipe. By setting `-o pipefail` the exit
-# status of the first (leftmost) non-zero command of the pipeline is propagated
-# to the end. However, the remaining commands in the pipeline still run and, if
-# the `-e` option is set, the script will exit at the end of the failing
-# pipeline (to be regarded as a single command).
-# When using grep within a pipeline with both `-o pipefail` and `-e` option set,
-# put the `|| [[ $? == 1 ]]` condition at the end of the pipeline.
-#
-#   grep "<expression>" "<target>" | command_2 | command_3 || [[ $? == 1 ]] 
-#
-# NOTE on -u option
-# ------------------
-# The existence operator ${:-} allows avoiding errors when testing variables by
-# providing a default value in case the variable is not defined or empty.
-#
-# result=${var:-value}
-#
-# If `var` is unset or null, `value` is substituted (and assigned to `results`).
-# Otherwise, the value of `var` is substituted and assigned.
-# ==============================================================================
-
 # Current date and time in "yyyy.mm.dd_HH.MM.SS" format
 now="$(date +"%Y.%m.%d_%H.%M.%S")"
-
-# Timestamp in seconds for execution-time computation
-# (use `time` in front of command lines running in background)
-start=$(date +%s)
 
 # For a friendlier use of colors in Bash
 red=$'\e[1;31m' # Red
@@ -90,19 +24,8 @@ mag=$'\e[1;35m' # Magenta
 cya=$'\e[1;36m' # Cyan
 end=$'\e[0m'
 
-# --- Function definition ------------------------------------------------------
+# --- Help message -------------------------------------------------------------
 
-# Default options
-ver="0.2.0"
-position="a-zA-Z"       # Keep all (i.e., do not filter for author position)
-doctype="AR"            # Both research articles and Reviews
-year_rgx="[0-9]{2}"     # All years (from 2000 to 2099)
-annexes=false
-tabular=false
-export=false
-destination="."
-
-# Help message
 _help_pubs=""
 read -d '' _help_pubs << EOM || true
 This utility script allows monitoring my production of scientific papers.
@@ -139,7 +62,16 @@ Additional Notes:
     ...
 EOM
 
-# --- Argument parsing ---------------------------------------------------------
+# --- Argument parsing and validity check --------------------------------------
+
+# Default options
+position="a-zA-Z"       # Keep all (i.e., do not filter for author position)
+doctype="AR"            # Both research articles and Reviews
+year_rgx="[0-9]{2}"     # All years (from 2000 to 2099)
+annexes=false
+tabular=false
+export=false
+destination="."
 
 # Flag Regex Pattern (FRP)
 frp="^-{1,2}[a-zA-Z0-9-]+"
@@ -245,9 +177,8 @@ done
 
 # --- Main program -------------------------------------------------------------
 
-debug=true
-
 echo
+debug=false
 counter=1
 
 while IFS= read -r project
